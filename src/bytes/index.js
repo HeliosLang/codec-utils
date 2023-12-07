e5b6913301e385a0626f62d34e9ba5728360d3cb
Christian Schmitz <cschmitz398@gmail.com>
2023-12-07 12:39:33 -0600

    export base32 encoding and decoding functions

--- src/bytes/index.js
+++ src/bytes/index.js
@@ -1,3 +1,10 @@
 export { ByteStream } from "./ByteStream.js"
 export { bytesToHex, hexToBytes } from "./base16.js"
+export {
+    DEFAULT_BASE32_ALPHABET,
+    decodeBase32,
+    encodeBase32,
+    encodeBase32Raw,
+    isValidBase32
+} from "./base32.js"
 export { padBytes, compareBytes } from "./ops.js"
