b6f1aff427686c053db63fc07fec2b3e0601284a
Christian Schmitz <cschmitz398@gmail.com>
2024-05-03 17:38:45 -0600

    exported isValidHex

--- src/bytes/index.js
+++ src/bytes/index.js
@@ -1,6 +1,6 @@
 export { toBytes } from "./ByteArrayLike.js"
 export { ByteStream } from "./ByteStream.js"
-export { bytesToHex, hexToBytes } from "./base16.js"
+export { bytesToHex, hexToBytes, isValidHex } from "./base16.js"
 export { Base32, decodeBase32, encodeBase32, isValidBase32 } from "./base32.js"
 export { Base64, decodeBase64, encodeBase64, isValidBase64 } from "./base64.js"
 export { compareBytes, equalsBytes, padBytes } from "./ops.js"
